c5867f366f846ab564d63430e780992fc74d3921
Hemanth <hemanthsingh11011@gmail.com>
2023-05-12 23:41:52 +0530

    feat: add snackbar

--- frontend/src/utils/GetAccessToken.tsx
+++ frontend/src/utils/GetAccessToken.tsx
@@ -1,12 +1,12 @@
 import Router from "next/router";
 
 const getToken = async () => {
-  let refreshToken = localStorage.getItem("refreshToken");
+  const refreshToken = localStorage.getItem("refreshToken");
   if (!refreshToken) {
     void Router.push("../");
     return false;
   }
-  let url = `http://${process.env.NEXT_PUBLIC_SERVER_URL}/users/refresh`;
+  const url: string|undefined = `http://${process.env.NEXT_PUBLIC_SERVER_URL}/users/refresh`;
   try {
     const response = await fetch(url, {
       method: "POST",
