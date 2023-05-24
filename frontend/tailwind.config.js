7de735e71b84214d0c82489d7dba44cac6d7158c
aditansh <aadityamahanta07@gmail.com>
2023-05-24 07:14:20 +0530

    fix: retarded tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,11 +6,11 @@ const config = {
       fontFamily: {
         spacegrostesk: ["Space Grotesk"],
         metropolis: ["Metropolis"],
-      },
+      }
     },
-    screens: {
-      xl: { max: "1400px" },
-    },
+    // screens: {
+    //   xl: { max: "1400px" },
+    // },
   },
   plugins: [require("@tailwindcss/forms"), require("flowbite/plugin")],
 };
